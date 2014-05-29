b82f1c5ab99719405c3c7dd412a79be31b7eb5c3
CarlosReyesStoneham <cer26@duke.edu>
2014-05-29 00:14:15 -0400

    added expressjs framework with basic routing set up and mongodb connected for basic reads from db

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var user_model = require('../models/users_model.js');
+
+mongoose.connect("mongodb://localhost:27017/test");
+
+router.get('/', function(req, res) {
+	mongoose.model('users').find({_id: "carlos"}, function(err, users) {
+		res.send(users[0]);
+	});
+});
+
+module.exports = router;
