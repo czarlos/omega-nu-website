8c0ae016ae85ad3bd9bc3819931ba3a6cfbb066e
CarlosReyesStoneham <cer26@duke.edu>
2014-05-30 02:00:26 -0400

    used ejs templating to reduce redundany in html, moved images, edited index routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var user_model = require('../models/users_model.js');
+
+//mongoose.connect("mongodb://localhost:27017/test");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+	mongoose.model('users').find(null, function(err, users) {
+		res.render('index', {team: users});
+	});
 });  
 
 module.exports = router;
