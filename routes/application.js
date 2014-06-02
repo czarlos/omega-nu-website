a587d7b7a6b7dd21cabf48c27d90607d334411b8
CarlosReyesStoneham <cer26@duke.edu>
2014-06-02 00:21:11 -0400

    form submission writes to mongodb

--- routes/application.js
+++ routes/application.js
@@ -4,21 +4,21 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connect("mongodb://localhost:27017/test");
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
 	res.render('application', function(err, html){
 		
 	});	
-	console.dir(req.body);
+	console.dir(req.body.first_name);
 
-	/*var winesSchema = new mongoose.Schema({
+	var winesSchema = new mongoose.Schema({
 		first_name: 'string',
 		last_name: 'string'
 	}); 
 	console.dir("real " + req.body.first_name);
 	var Money = mongoose.model('Book', winesSchema);
 	var small = new Money({
-		first_name: "2Chainz",
-		last_name: "money"
+		first_name: req.body.first_name,
+		last_name: req.body.last_name
 	});
 	small.save(function (err) {
 		if (err) {
@@ -27,7 +27,7 @@ router.get('/', function(req, res) {
 		else {
 			res.redirect('/');
 		}	
-	}); */
+	});
 });
 
 module.exports = router;
