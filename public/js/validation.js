ab38332f5476869e6d4c2fe1c9d8ad006224ebee
CarlosReyesStoneham <cer26@duke.edu>
2014-06-03 01:13:43 -0400

    added form validation for application page

---
+++ public/js/validation.js
@@ -0,0 +1,50 @@
+$('form').validate({
+	rules: {
+		first_name: {
+			minlength: 3,
+			maxlength: 15,
+			required: true
+		},
+		last_name: {
+			minlength: 3,
+			maxlength: 15,
+			required: true
+		},
+		github_handle: {
+			minlength: 2,
+			maxlength: 15,
+			required: true
+		},
+		email: {
+			minlength: 3,
+			maxlength: 15,
+			email: true,
+			required: true
+		},
+		website: {
+			minlength: 3,
+			maxlength: 15,
+			url: true
+		},
+		linkedin: {
+			minlength: 3,
+			maxlength: 15,
+			url: true 
+		}
+	},
+	highlight: function(element) {
+		$(element).closest('.form-group').addClass('has-error');
+	},
+	unhighlight: function(element) {
+		$(element).closest('.form-group').removeClass('has-error');
+	},
+	errorElement: 'span',
+	errorClass: 'has-error',
+	errorPlacement: function(error, element) {
+		if(element.parent('.input-group').length) {
+			error.insertAfter(element.parent());
+		} else {
+			error.insertAfter(element);
+		}
+	}
+});
