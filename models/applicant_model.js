4db8476cbc0b32763844c87be6f9dbc2ec0485fe
CarlosReyesStoneham <cer26@duke.edu>
2014-06-02 20:57:50 -0400

    cleaned up the code, separating out the model from the controller

--- models/applicant_model.js
+++ models/applicant_model.js
@@ -1,22 +1,16 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var applicant_schema = new Schema({
-    firstname : String,
-	lastname : String
-});
+var applicantSchema = new mongoose.Schema({
+	first_name: String,
+	last_name: String,
+	github_handle: String,
+	email: String,
+	linkedin: String,
+	website: String,
+	about: String,
+	resume: Buffer
+}); 
 
-var Applicant = mongoose.model('Applicants', applicant_schema);
+var Applicant = mongoose.model('Applicants', applicantSchema);
 
-var new_applicant = new Applicant({
-	firstname : "2Chainz",
-	lastname : "Money",
-	collection : 'applicants'
-});
-
-new_applicant.save(function(err, new_applicant) {
-	if (err) {
-		return console.error(err);		
-	}
-});
-
+module.exports = Applicant;
--- routes/application.js
+++ routes/application.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+Applicant = require('../models/applicant_model.js');
 
 var db = mongoose.connect("mongodb://localhost:27017/test");
 
 router.post('/', function(req, res) {
 	res.render('application', function(err, html){
 		
-	});	
+	});
 
-	var winesSchema = new mongoose.Schema({
-		first_name: String,
-		last_name: String,
-		github_handle: String,
-		email: String,
-		linkedin: String,
-		website: String,
-		about: String,
-		resume: Buffer
-	}); 
-	var Applicant = mongoose.model('Applicants', winesSchema);
 	var applicant = new Applicant({
 		first_name: req.body.first_name,
 		last_name: req.body.last_name,
