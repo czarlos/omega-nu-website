62a3bdca0b2627bf642200be6a4037eff98baff3
CarlosReyesStoneham <cer26@duke.edu>
2014-06-06 19:31:22 -0400

    added access control allow origin so that omeganu.us domain can access mongo

--- app.js
+++ app.js
@@ -29,8 +29,22 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/application', application);
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.omeganu.us');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+});
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
