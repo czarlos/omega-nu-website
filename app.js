89aac16b47beb5dec2ddb7777938c05ad896c4b6
CarlosReyesStoneham <cer26@duke.edu>
2014-06-06 21:34:55 -0400

    cross domain is safer

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', routes);
 
 /*NOTE: It is VERY important that this isn't moved, placement is key*/
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://www.omeganu.us');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
  
