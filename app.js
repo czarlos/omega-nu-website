e988d6a739233c9cf3178a1c559f8c71cf986f09
CarlosReyesStoneham <cer26@duke.edu>
2014-06-06 20:33:08 -0400

    cross domain requests work, placement of the header stuff is critical

--- app.js
+++ app.js
@@ -26,11 +26,27 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+/*NOTE: It is VERY important that this isn't moved, placement is key*/
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 app.use('/users', users);
 app.use('/application', application);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
