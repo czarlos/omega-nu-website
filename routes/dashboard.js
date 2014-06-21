29a425afbda651635dba8029bfd3fb02330c68d3
czarlos <cer26@duke.edu>
2014-06-21 00:03:57 -0400

    added route to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,52 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-Dashboard = require('../models/dashboard_model.js');
 
-router.post('/', function(req, res) {
-	res.render('dashboard', function(err, html) {});
-
-	var user = req.body.first_name + " " + req.body.last_name;
-
-	var dashboard = new Dashboard({
-		_id: "",
-		person: user,
-		company: checkValue("company", user, req.body.organization),
-		concentrations: checkValue("concentrations", user, ""),
-		skills: checkValue("skills", user, req.body.skills),
-		bio: checkValue("bio", user, req.body.bio),
-		interests: checkValue("coding", user, "")
-	});
-
-	Dashboard.findOne({person:user}).update(dashboard.toObject(), function (err) {
-		if (err) {
-			console.dir("err");
-		}
-		else {
-			res.redirect('/');
-		}
-	});
+router.get('/', function(req, res) {
+	res.render('dashboard', {title:'This money'});
 });
-  
-function getValue (user, field) {
-	Dashboard.findOne({person: user}, function(err, doc) {
-		if (err) {
-			console.dir(err);
-		}
-		else {
-			console.dir(doc[field]);
-			return doc[field];
-		}
-	});
-}
 
-function checkValue (field, user, input) {
-	if (input !== null && input !== "") {
-		return input;
-	}
-	else {
-		getValue(user, field);
-	}
-}
-  
 module.exports = router;
