e7f21b74ba81491a983bc4105a918f2b0546688d
czarlos <cer26@duke.edu>
2014-06-21 00:17:00 -0400

    added username to dashboard

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-router.get('/', function(req, res) {
-	res.render('dashboard', {name: "Carlos Reyes"});
+router.get('/login', function(req, res) {
+	res.render('login', {title:'This money'});
 });
 
 module.exports = router;
