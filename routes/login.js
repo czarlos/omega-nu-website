431a077f35bbe56d7d8de9a66650c91f438228ae
czarlos <cer26@duke.edu>
2014-06-24 01:50:29 -0400

    added outline for login route

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var user_model = require('../models/users_model.js');
 
 router.post('/', function(req, res) {
+	var user = req.body.user;
+	var password = req.body.password;
+
+	if (isValidCombo(email, password)) {
+		res.redirect('/dashboard');
+	}
+	else {
+		res.redirect('/');
+	}
 	res.send("this");
 });
+
+function isValidCombo(email, password) {
+
+}
 
 module.exports = router;
