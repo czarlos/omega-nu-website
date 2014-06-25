22a5b304bdf8524bd6d500a21cccb3aecf98f866
czarlos <cer26@duke.edu>
2014-06-25 00:15:36 -0400

    consolidated login route to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res) { console.dir("THIS MONEY");
 	res.render('dashboard', {name: "Carlos Reyes"});
 });
 
