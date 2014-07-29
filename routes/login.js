cf6def42ff75992d9cbc12e968904ab267f1a8f4
czarlos <cer26@duke.edu>
2014-07-28 23:07:30 -0400

    added better handling for bad user/pass

--- routes/login.js
+++ routes/login.js
@@ -11,14 +11,17 @@ router.post('/', function(req, res) {
 function checkPassword (req, res) {
 	var un = req.body.user;
 	var pass = req.body.password;
-	
+
 	var hashed_password = password_hash.generate(pass);
-	
+
 	mongoose.model('users').findOne({username:un}, function(err, doc) {
-		// We shouldn't need to do this	
+		// We shouldn't need to do this
 		var json_string = JSON.stringify(doc);
 		var subject = JSON.parse(json_string);
-		if (un === subject['username'] && pass === subject['password']) {
+        if (subject==null) {
+            res.redirect('/');
+        }
+		else if (un === subject['username'] && pass === subject['password']) {
 			res.render('dashboard', {name:req.body.user});
 		}
 		else {
