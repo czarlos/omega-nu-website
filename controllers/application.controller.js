78ab05f6c2fabdb5d7cea2875e4c868c4e93639f
czarlos <cer26@duke.edu>
2015-01-24 00:02:53 -0500

    added new gitignore and moved application controller logic

---
+++ controllers/application.controller.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Applicant = require('../models/applicant_model.js');
+var db = mongoose.connect("mongodb://localhost:27017/test");
+
+exports.newApplicant = function(req, res) {
+	res.render('application', function(err, html){
+
+	});
+
+	var applicant = new Applicant({
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		github_handle: req.body.github_handle,
+		email: req.body.email,
+		linkedin: req.body.linkedin,
+		website: req.body.website,
+		about: req.body.about,
+		resume: req.body.resume
+	});
+
+	applicant.save(function (err) {
+		if (err) {
+			return handleError(err);
+		}
+		else {
+			res.redirect('/');
+		}
+	});
+};
+
--- routes/application.js
+++ routes/application.js
@@ -1,33 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-Applicant = require('../models/applicant_model.js');
+var application = require('../controllers/application.controller');
 
-var db = mongoose.connect("mongodb://localhost:27017/test");
-
-router.post('/', function(req, res) {
-	res.render('application', function(err, html){
-		
-	});
-
-	var applicant = new Applicant({
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		github_handle: req.body.github_handle,
-		email: req.body.email,
-		linkedin: req.body.linkedin,
-		website: req.body.website,
-		about: req.body.about,
-		resume: req.body.resume
-	});
-	applicant.save(function (err) {
-		if (err) {
-			return handleError(err);
-		}
-		else {
-			res.redirect('/');
-		}	
-	});
-});
+router.post('/', application.newApplicant);
 
 module.exports = router;
