a24d549ab3784dba502d2f429886e82caa5dc12c
czarlos <cer26@duke.edu>
2015-03-30 04:05:11 +0000

    update release

--- routes/application.js
+++ routes/application.js
@@ -2,35 +2,6 @@ var express = require('express');
 var router = express.Router();
 var application = require('../controllers/application.controller');
 
-//var db = mongoose.connect("mongodb://localhost:27017/test");
-
-var mongoURI = process.env.MONGOLAB;
-var mongooseURI = uriUtil.formatMongoose(mongoURI);
-var db = mongoose.connect(mongooseURI);
-
-router.post('/', function(req, res) {
-	res.render('application', function(err, html){
-
-	});
-
-	var applicant = new Applicant({
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		github_handle: req.body.github_handle,
-		email: req.body.email,
-		linkedin: req.body.linkedin,
-		website: req.body.website,
-		about: req.body.about,
-		resume: req.body.resume
-	});
-	applicant.save(function (err) {
-		if (err) {
-			return handleError(err);
-		}
-		else {
-			res.redirect('/');
-		}
-	});
-});
+router.post('/', application.newApplicant);
 
 module.exports = router;
